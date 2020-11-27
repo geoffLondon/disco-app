91a1f99366c666e98db57774d02747470c7889bb
geoff <g.griffiths@resilientplc.com>
2020-11-27 21:12:13 +0000

    Setup initial Grid layout.

--- src/pages/front-page.js
+++ src/pages/front-page.js
@@ -3,14 +3,30 @@ import Header from '../components/header'
 import AddSong from '../components/add-song'
 import SongList from '../components/song-list'
 import SongPlayer from '../components/song-player'
+import { Grid, withStyles } from '@material-ui/core'
+
+const styles = theme => ({
+  profileIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  menu: {
+    marginTop: theme.spacing(5),
+  },
+})
 
 const FrontPage = () => {
   return <div>
     <Header/>
-    <AddSong/>
-    <SongList/>
-    <SongPlayer/>
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={7}>
+        <AddSong/>
+        <SongList/>
+      </Grid>
+      <Grid item xs={12} md={5}>
+        <SongPlayer/>
+      </Grid>
+    </Grid>
   </div>
 }
 
-export default FrontPage
+export default withStyles(styles)(FrontPage)
